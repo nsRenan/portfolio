b1abacd9cadcd6daab63fcba4e9aee3bb276ab44
Renan Nobre <renannobresan@gmail.com>
2024-01-28 11:53:53 -0300

    Resolução Bug

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,29 +1,65 @@
-import Link from "next/link";
+"use client";
+import { Link, animateScroll as scroll, } from "react-scroll/modules";
 import { SiLinkedin, SiGithub } from "react-icons/si";
 import styles from "./navbar.module.css";
 import { RiMailSendLine } from "react-icons/ri";
-import renanLogo from '/public/renan-logo.svg'
+import renanLogo from "/public/renan-logo.svg";
 import Image from "next/image";
 import { BarraLateral } from "../BarraLateral";
 
 export function Navbar() {
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  };
   return (
     <header className={styles.main}>
       <div className={styles.nome}>
-        <Link href="/"> <Image width={44}  src={renanLogo} alt={"logo"} /> </Link>
+        <a onClick={scrollToTop} style={{ cursor: "pointer" }}>
+          <Image width={44} src={renanLogo} alt={"logo"} />
+        </a>
       </div>
       <menu className={styles.menu}>
-        <Link href="#sobre"> Sobre</Link>
-        <Link href="#projetos"> Projetos</Link>
-        <Link href="#trajetoria"> Trajetória</Link>
+        <Link
+          activeClass="active"
+          to="sobre"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+          style={{ cursor: "pointer" }}
+        >
+          Sobre
+        </Link>
+        <Link
+          activeClass="active"
+          to="projetos"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+          style={{ cursor: "pointer" }}
+        >
+          Projetos
+        </Link>
+        <Link
+          activeClass="active"
+          to="trajetoria"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+          style={{ cursor: "pointer" }}
+        >
+          Trajetória
+        </Link>
       </menu>
       <div className={styles.menuBotoes}>
-        <Link target="_blank" href="https://www.linkedin.com/in/renan-nobre/">
+        <a target="_blank" href="https://www.linkedin.com/in/renan-nobre/">
           <SiLinkedin /> Linkedin
-        </Link>
-        <Link target="_blank" href="https://github.com/nsRenan">
+        </a>
+        <a target="_blank" href="https://github.com/nsRenan">
           <SiGithub /> Github
-        </Link>
+        </a>
         <button className={styles.contato}>
           <a href="mailto:contact.renannobre@gmail.com">
             <RiMailSendLine />
@@ -31,7 +67,7 @@ export function Navbar() {
           </a>
         </button>
       </div>
-      <BarraLateral/>
+      <BarraLateral />
     </header>
   );
 }
