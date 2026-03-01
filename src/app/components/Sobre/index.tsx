456328519c52acda3dc2e86d0825209d5c8ae22e
Renan Nobre <renannobresan@gmail.com>
2026-03-01 00:43:41 -0300

    feat: update profile image and enhance styles with new border radius and background gradient

--- src/app/components/Sobre/index.tsx
+++ src/app/components/Sobre/index.tsx
@@ -2,7 +2,7 @@
 import styles from "./sobre.module.css";
 import { FiChevronsRight } from "react-icons/fi";
 import Image from "next/image";
-import minhaFoto from "/public/imagens/renanPerfil.png";
+import minhaFoto from "/public/imagens/fotoPerfil.jpg";
 import { Habilidades } from "../Habilidades";
 import { useState } from "react";
 
